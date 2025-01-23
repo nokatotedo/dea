8155f32bf2a41caf6159d2e3c44b6eff6355ae6b
nokatotedo <dwikypanduaqri22@gmail.com>
2025-01-23 08:52:33 +0700

    feat: dashboard kece

--- index.php
+++ index.php
@@ -134,9 +134,42 @@
         </nav>
       </header>
       <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-12 d-flex justify-content-center align-items-center" style="height: calc(100vh - 119px) !important;">
-            Ini Dashboard
+        <div class="card">
+          <div class="card-body d-flex flex-column gap-4">
+            <div class="text-center fs-6 fw-bold">PENERAPAN METODE MOORA (MULTI OBJECTIVE OPTIMIZATION 
+              ON THE BASIS OF RATIO ANALYSIS) DALAM SISTEM PENDUKUNG 
+              KEPUTUSAN BERBASIS MULTI CRITERIA DECISION MAKING 
+              (MCDM) DALAM MENENTUKAN LOKASI STRATEGIS 
+              PEMBANGUNAN KOMPLEK PERUMAHAN PT GRAHA CITRA MULIA 
+              DI KABUPATEN SOLOK</div>
+            <div>
+              <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="01.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="02.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="03.jpg" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
